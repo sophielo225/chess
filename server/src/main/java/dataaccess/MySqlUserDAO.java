5653cfe1ee4f5e752c42c3f840fa815fc98a5b23
Shu-Fei Lo <shufeilo@byu.edu>
2025-03-07 10:41:30 -0700

    Update MySqlUserDAO

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,8 +1,63 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.UserData;
 
+import java.sql.SQLException;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class MySqlUserDAO implements UserDAO{
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS user (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`username`),
+              INDEX(username)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    public MySqlUserDAO() throws ResponseException, DataAccessException {
+        configureDatabase();
+    }
+
+    private void configureDatabase() throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    private void executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) {
+                        ps.setString(i + 1, p);
+                    } else if (param == null) {
+                        ps.setNull(i + 1, NULL);
+                    }
+                }
+                ps.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement,
+                    e.getMessage()));
+        }
+    }
+
     @Override
     public void clear() {
 
