bf8bcc37dfa6d30f67ff5052f95d51400b98639c
Shu-Fei Lo <shufeilo@byu.edu>
2025-03-09 20:13:53 -0600

    Update MySqlUserDAO and MySqlAuthDAO

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -1,35 +1,92 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.AuthData;
 
-public class MySqlAuthDAO implements AuthDAO{
-    @Override
-    public void clear() {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class MySqlAuthDAO implements SqlAuthDAO, SqlDAO {
+
+    public MySqlAuthDAO() throws ResponseException {
+        configureDatabase(createAuths);
     }
 
     @Override
-    public AuthData createAuth(String token, String username) throws DataAccessException {
+    public void clear() throws ResponseException {
+        var statement = "TRUNCATE auths";
+        executeUpdate(statement);
+    }
+
+    @Override
+    public AuthData createAuth(String token, String username) throws ResponseException {
+        var statement = "INSERT INTO auths (token, username) VALUES (?, ?)";
+        executeUpdate(statement, token, username);
+        return new AuthData(token, username);
+    }
+
+    private AuthData readAuth(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var token = rs.getString("token");
+        return new AuthData(token, username);
+    }
+
+    @Override
+    public AuthData getAuth(String username) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, token FROM auths WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
-    public AuthData getAuth(String username) throws DataAccessException {
+    public void deleteAuth(String username) throws ResponseException {
+        var statement = "DELETE FROM auths WHERE username=?";
+        executeUpdate(statement, username);
+    }
+
+    @Override
+    public String isAuthorized(String authToken) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT token, username FROM auths WHERE token=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, authToken);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readAuth(rs).username();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     @Override
-    public void deleteAuth(String username) throws DataAccessException {
-
-    }
-
-    @Override
-    public String isAuthorized(String authToken) {
-        return "";
-    }
-
-    @Override
-    public int getAuthSize() {
+    public int getAuthSize() throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT COUNT(*) FROM auths";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getInt(1);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+        }
         return 0;
     }
 }
--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -5,88 +5,40 @@ import exception.ResponseException;
 import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 
-import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
-import static java.sql.Types.NULL;
+public class MySqlUserDAO implements SqlUserDAO, SqlDAO {
 
-public class MySqlUserDAO implements UserDAO{
-
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS user (
-              `username` varchar(256) NOT NULL,
-              `password` varchar(256) NOT NULL,
-              `email` varchar(256) NOT NULL,
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`username`),
-              INDEX(username)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
-
-    public MySqlUserDAO() throws ResponseException, DataAccessException {
-        configureDatabase();
-    }
-
-    private void configureDatabase() throws ResponseException, DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex) {
-            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-    }
-
-    private void executeUpdate(String statement, Object... params) throws ResponseException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) {
-                        ps.setString(i + 1, p);
-                    } else if (param == null) {
-                        ps.setNull(i + 1, NULL);
-                    }
-                }
-                ps.executeUpdate();
-            }
-        } catch (SQLException | DataAccessException e) {
-            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement,
-                    e.getMessage()));
-        }
+    public MySqlUserDAO() throws ResponseException {
+        configureDatabase(createUsers);
     }
 
     @Override
     public void clear() throws ResponseException {
-        var statement = "TRUNCATE user";
+        var statement = "TRUNCATE users";
         executeUpdate(statement);
     }
 
     @Override
     public UserData createUser(String username, String password, String email) throws ResponseException {
-        var statement = "INSERT INTO user (username, password, email, json) VALUES (?, ?, ?, ?)";
+        var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-        var userData = new UserData(username, hashedPassword, email);
-        var json = new Gson().toJson(userData);
-        executeUpdate(statement, userData.username(), userData.password(), userData.email(), json);
-        return new UserData(userData.username(), userData.password(), userData.email());
+        executeUpdate(statement, username, hashedPassword, email);
+        return new UserData(username, password, email);
     }
 
     private UserData readUser(ResultSet rs) throws SQLException {
-        var json = rs.getString("json");
-        return new Gson().fromJson(json, UserData.class);
+        var username = rs.getString("username");
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+        return new UserData(username, password, email);
     }
 
     @Override
     public UserData getUser(String username, String password) throws ResponseException {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT username, json FROM user WHERE username=?";
+            var statement = "SELECT username, password, email FROM users WHERE username=?";
             try (var ps = conn.prepareStatement(statement)) {
                 ps.setString(1, username);
                 try (var rs = ps.executeQuery()) {
@@ -99,8 +51,6 @@ public class MySqlUserDAO implements UserDAO{
                             return null;
                         }
                     }
-                } catch (SQLException e) {
-                    throw new DataAccessException("Invalid username");
                 }
             }
         } catch (Exception e) {
@@ -112,7 +62,7 @@ public class MySqlUserDAO implements UserDAO{
     @Override
     public int getUserSize() throws ResponseException {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "SELECT COUNT(*) FROM user";
+            var statement = "SELECT COUNT(*) FROM users";
             try (var ps = conn.prepareStatement(statement)) {
                 try (var rs = ps.executeQuery()) {
                     if (rs.next()) {
