e57704a2002229215702f592a28ed23f7cb4363e
Shu-Fei Lo <shufeilo@byu.edu>
2025-04-06 01:11:04 -0600

    Update ConnectionManager

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,39 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String userName, Session session) {
+        var connection = new Connection(userName, session);
+        connections.put(userName, connection);
+    }
+
+    public void remove(String userName) {
+        connections.remove(userName);
+    }
+
+    public void broadcast(String excludeUserName, ServerMessage notification) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.userName.equals(excludeUserName)) {
+                    c.send(notification.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.userName);
+        }
+    }
+}
